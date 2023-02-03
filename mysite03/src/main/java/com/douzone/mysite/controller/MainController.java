94268baffe2bf8e6a0c531eb83d849c7bd39f598
rhkddms3501 <rhkddms3501@naver.com>
2023-02-03 09:59:12 +0900

    MessageConvert #1

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -2,13 +2,39 @@ package com.douzone.mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.mysite.vo.UserVo;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("메인 컨트롤러");
 		return "main/index";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg01")
+	public String message01() {
+		return "Hello World";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg02")
+	public String message02() {
+		return "하이루";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/msg03")
+	public Object message03() {
+		UserVo vo = new UserVo();
+		vo.setNo(1L);
+		vo.setName("신짱구");
+		vo.setEmail("shin@email.com");
+		return vo;
 	}
 
 }
