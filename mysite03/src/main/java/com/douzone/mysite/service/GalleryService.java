d2b5cd018e86e917e2b665e0001ca044de70583d
rhkddms3501 <rhkddms3501@naver.com>
2023-02-07 15:35:00 +0900

    gallery #2

---
+++ mysite03/src/main/java/com/douzone/mysite/service/GalleryService.java
@@ -0,0 +1,22 @@
+package com.douzone.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.douzone.mysite.vo.GalleryVo;
+
+@Service
+public class GalleryService {
+
+	public List<GalleryVo> getImages(){
+		return null;
+	}
+	
+	public void removeImage(Long no) {
+	}
+	
+	public void addImage(GalleryVo vo) {
+	}
+	
+}
